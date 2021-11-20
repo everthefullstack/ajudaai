cca80e3c11c56d1d43e995872995b58d8f2e5e67
Giovani Lima <giovanialmeidadelima@gmail.com>
2021-11-20 17:10:44 -0300

    teste no config

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ class configuracoes():
 
     # Deleta todo e qualquer cache para carregar as modificações feitas no programa
     SEND_FILE_MAX_AGE_DEFAULT = 0
-    SECRET_KEY = uuid1().hex
-    JWT_SECRET_KEY = uuid1().hex
+    SECRET_KEY = "ajudaai"
+    JWT_SECRET_KEY = "ajudaai"
     ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
     UPLOAD_FOLDER = "/static/img"
     URL_APP = "https://appajudaai.herokuapp.com"
