0dba84895850b088ef6dfe4ed73f8dd83e27ca06
Giovani Lima <giovanialmeidadelima@gmail.com>
2021-11-20 13:10:14 -0300

    alteração teste

--- model/base_model.py
+++ model/base_model.py
@@ -5,7 +5,7 @@ class BaseModel(Model):
     class Meta:
         
         #database = SqliteDatabase('database/ajudaai.db', pragmas={'foreign_keys': 1})
-        database = PostgresqlDatabase(database="da5b7sels5sf33", 
+        database = PostgresqlDatabase("da5b7sels5sf33", 
                                       user="zcweuekvmmdpsh", 
                                       password="1e84a5310ec6b5e477b92c93c1399b50b76a8537f281689ac93bef4f88d8b2fc",
                                       host= "ec2-35-168-80-116.compute-1.amazonaws.com", 
